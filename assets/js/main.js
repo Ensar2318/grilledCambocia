36e7ce7158da7f4a78284b59122bde8f2742c9ff
Ensar2318 <68289958+Ensar2318@users.noreply.github.com>
2022-08-21 20:52:43 +0300

    Add files via upload
    asd

---
+++ assets/js/main.js
@@ -0,0 +1,101 @@
+// Header ve footer load işlemi
+$(document).ready(function() {
+    $("#header").load("header.html");
+    $("#footer").load("footer.html");
+});
+
+$(".menu-add-btn").click(function(e) {
+    e.preventDefault();
+
+    var height = $(this).parent().parent().children(".detail-wrap").prop("scrollHeight");
+    if ($(this).hasClass("active")) {
+        $(this).parent().parent().children(".detail-wrap").css("max-height", 0);
+    } else {
+        $(this).parent().parent().children(".detail-wrap").css("max-height", height);
+    }
+    $(this).toggleClass("active");
+
+});
+
+$(window).scroll(function() {
+    let current = "";
+    $(".banner-wrap").each(function(index, element) {
+        // element == this
+        const bannerTop = element.offsetTop;
+        if (pageYOffset >= bannerTop + 300) {
+            current = element.getAttribute("id");
+        }
+        $(".nav-link").each(function(index, element) {
+            // element == this
+
+            if ($(element).attr("href").split("#")[1] == current) {
+                $(element).addClass("active");
+            } else {
+                $(element).removeClass("active");
+            }
+        });
+
+    });
+});
+
+// only mobile work swiper
+var init = false;
+
+function swiperCard() {
+    if (window.innerWidth <= 768) {
+        if (!init) {
+            init = true;
+            swiper = new Swiper(".navbarSwiper", {
+                slidesPerView: 3,
+                spaceBetween: 10,
+            });
+        }
+    } else if (init) {
+        swiper.destroy();
+        init = false;
+    }
+}
+swiperCard();
+window.addEventListener("resize", swiperCard);
+// only mobile work swiper
+
+$(".mobile-cart").click(function (e) { 
+    e.preventDefault();
+    $(".mobile-card-checkout").addClass("active");
+    $("body").css("overflow","hidden");
+});
+$(".card-checkout-wrap .close-btn").click(function (e) { 
+    e.preventDefault();
+    $(".mobile-card-checkout").removeClass("active");
+    $("body").css("overflow","");
+});
+
+
+//#####Swiper Örneği######
+
+// var announcementSwiper = new Swiper(".announcementSwiper", {
+//     slidesPerView: 3,
+//     spaceBetween: 20,
+//     loop: true,
+//     loopFillGroupWithBlank: true,
+//     centeredSlides: true,
+//     breakpoints: {
+
+//         768: {
+//             slidesPerView: 3,
+//             spaceBetween: 50,
+//         },
+//         1024: {
+//             slidesPerView: 3,
+//             spaceBetween: 50,
+//         },
+//     },
+//     pagination: {
+//         el: ".swiper-pagination",
+//         clickable: true,
+//     },
+//     navigation: {
+//         nextEl: ".announcementSwiper-button-next",
+//         prevEl: ".announcementSwiper-button-prev",
+//     },
+// });
---
+++ assets/vendor/icomoon/ie7/ie7.js
@@ -0,0 +1,64 @@
+/* To avoid CSS expressions while still supporting IE 7 and IE 6, use this script */
+/* The script tag referencing this file must be placed before the ending body tag. */
+
+/* Use conditional comments in order to target IE 7 and older:
+	<!--[if lt IE 8]><!-->
+	<script src="ie7/ie7.js"></script>
+	<!--<![endif]-->
+*/
+
+(function() {
+	function addIcon(el, entity) {
+		var html = el.innerHTML;
+		el.innerHTML = '<span style="font-family: \'icomoon\'">' + entity + '</span>' + html;
+	}
+	var icons = {
+		'icon-bag-clock': '&#xe900;',
+		'icon-succes-paper': '&#xe906;',
+		'icon-succes-card': '&#xe90f;',
+		'icon-megaphone': '&#xe914;',
+		'icon-megaphone-big': '&#xe915;',
+		'icon-motor': '&#xe916;',
+		'icon-notification': '&#xe917;',
+		'icon-phone': '&#xe918;',
+		'icon-plus': '&#xe919;',
+		'icon-plus-flat': '&#xe91a;',
+		'icon-search': '&#xe91b;',
+		'icon-shut-eye': '&#xe91c;',
+		'icon-star-flat': '&#xe91d;',
+		'icon-star-radio': '&#xe91e;',
+		'icon-store': '&#xe91f;',
+		'icon-tick': '&#xe920;',
+		'icon-trash': '&#xe921;',
+		'icon-user': '&#xe922;',
+		'icon-minus': '&#xe923;',
+		'icon-apple': '&#xe924;',
+		'icon-Arrow-icon-left': '&#xe925;',
+		'icon-Arrow-icon-right': '&#xe926;',
+		'icon-bag': '&#xe927;',
+		'icon-clock': '&#xe928;',
+		'icon-credit-card': '&#xe929;',
+		'icon-cros': '&#xe92a;',
+		'icon-default-key': '&#xe92b;',
+		'icon-exit': '&#xe92c;',
+		'icon-gear': '&#xe92d;',
+		'icon-headphone': '&#xe92e;',
+		'icon-key-input': '&#xe92f;',
+		'icon-mail': '&#xe930;',
+		'icon-map-pin': '&#xe931;',
+		'0': 0
+		},
+		els = document.getElementsByTagName('*'),
+		i, c, el;
+	for (i = 0; ; i += 1) {
+		el = els[i];
+		if(!el) {
+			break;
+		}
+		c = el.className;
+		c = c.match(/icon-[^\s'"]+/);
+		if (c && icons[c[0]]) {
+			addIcon(el, icons[c[0]]);
+		}
+	}
+}());
